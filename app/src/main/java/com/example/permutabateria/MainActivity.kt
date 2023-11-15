771485a326bece61c08869e6bb0bfcb1cf3315b7
gabriel barbosa <gabriel.barbosa.03@edu.unipar.br>
2023-11-15 13:39:19 -0300

    Final

--- app/src/main/java/com/example/permutabateria/MainActivity.kt
+++ app/src/main/java/com/example/permutabateria/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.permutabateria
-
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.ACTION_BATTERY_CHANGED
+import android.content.Intent.ACTION_BOOT_COMPLETED
+import android.content.IntentFilter
+import android.graphics.Color
+import android.os.BatteryManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.permutabateria.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+
+    private val handler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+
+            val tv: TextView = binding.nivelBateria;
+
+            if(msg.obj.toString().toFloat() > 70.0){
+                tv.setTextColor(Color.GREEN)
+            }else if(msg.obj.toString().toFloat() < 15.0){
+                tv.setTextColor(Color.RED)
+            }else{
+                tv.setTextColor(Color.BLACK)
+            }
+
+            tv.setText("O nível da bateria é: " + msg.obj)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,35 +49,23 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val intentFilter: IntentFilter = IntentFilter(ACTION_BATTERY_CHANGED);
+        intentFilter.addAction(ACTION_BOOT_COMPLETED);
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val receiver: BroadcastReceiver = object : BroadcastReceiver() {
+
+            override fun onReceive(context: Context, intent: Intent) {
+                val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+                val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+                var nivel = level * 100 / scale.toFloat();
+                val message = Message()
+
+                message.obj = nivel;
+
+                handler.sendMessage(message);
+            }
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        registerReceiver(receiver, intentFilter)
     }
 }
